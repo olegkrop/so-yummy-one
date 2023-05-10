4a26711832603413a9c7ccce1d3e29b9f3375508
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-05-10 08:42:57 -0400

    Update index.js

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,18 +1,13 @@
 const register = require("./register");
 const logout = require("./logout");
 const getCurrent = require("./getCurrent");
-const updateSubscription = require("./updateSubscription");
 const login = require("./login");
 const updateAvatar = require("./updateAvatar");
-const verifyEmail = require("./verifyEmail");
-const resendEmail = require("./resendEmail");
+
 module.exports = {
   register,
   login,
   logout,
   getCurrent,
-  updateSubscription,
   updateAvatar,
-  verifyEmail,
-  resendEmail,
 };
