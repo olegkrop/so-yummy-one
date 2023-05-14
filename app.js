8216fa6f2525352b023836e23b5cc1cbb978b513
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-05-14 21:14:11 +0300

    add:swagger

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 const authRouter = require("./routes/api/auth");
 const recipesRouter = require("./routes/api/recipes");
 const app = express();
@@ -13,6 +16,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/auth", authRouter);
 app.use("/recipes", recipesRouter);
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const app = require('./app')
-const mongoose = require('mongoose');
+const app = require("./app");
+const mongoose = require("mongoose");
 
 const { DB_HOST, PORT } = process.env;
 
-mongoose.connect(DB_HOST)
+mongoose
+  .connect(DB_HOST)
   .then(() => {
     app.listen(PORT);
-    console.log("Database connection successful")
+    console.log("Database connection successful");
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error.message);
     process.exit(1);
-  })
+  });
