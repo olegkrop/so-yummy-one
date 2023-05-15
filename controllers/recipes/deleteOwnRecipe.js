23f927836bf391b97e36071bfb196d2e808cafac
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-05-15 07:38:43 -0400

    c

--- controllers/recipes/deleteOwnRecipe.js
+++ controllers/recipes/deleteOwnRecipe.js
@@ -2,12 +2,8 @@
 const Recipe = require("../../models/recipe");
 
 const deleteOwnRecipe = async (req, res, next) => {
-  //   const { _id } = req.user;
   const recipeId = req.params.id;
   await Recipe.findByIdAndDelete(recipeId);
-
-  //   const recipe = await Recipe.findById(_id);
-  //   await recipe.remove();
 
   res.json({
     status: "success",
