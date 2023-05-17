83f45f6606a354b7cd961486241c54052e045040
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-05-17 08:12:14 -0400

    з

--- schemas/loginSchema.js
+++ schemas/loginSchema.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const emailRegexp =
-  /^[A-Za-z0-9][A-Za-z0-9._%+-]*@[A-Za-z0-9][A-Za-z0-9.-]+\.[A-Za-z]{1,}$/;
+  /^[A-Za-z0-9][A-Za-z0-9._%+-]*@[A-Za-z0-9][A-Za-z0-9.-]*\.?[A-Za-z]{2,}\.[A-Za-z]{1,}$/;
 
 const loginSchema = Joi.object({
   email: Joi.string().required().pattern(emailRegexp).messages({
