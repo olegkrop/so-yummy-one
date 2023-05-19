5b5570543f652d365f2d85c47238285b201966ee
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-05-19 15:00:39 -0400

    favoriteUpdate

--- controllers/shopping-list/addToShoppingList.js
+++ controllers/shopping-list/addToShoppingList.js
@@ -1,3 +1,37 @@
-// const User = require("../../models/user");
+const User = require("../../models/user");
 
-// const addToShoppingList = async (req, res) => {};
+const addToShoppingList = async (req, res) => {
+  const { _id: userId } = req.user;
+
+  // отримуємо інгредієнт від користувача
+  const { _id, measure } = req.body;
+  const ingredient = { _id, measure };
+
+  // Перевіряємо чи передані всі дані
+  if (!_id || !measure) {
+    res.status(400);
+    throw new Error("Controller: Please provide all required fields");
+  }
+
+  // Оновлюємо користувача
+  // якщо потрібно щоб id не могли дублюватись використовуємо $addToSet: замість $push:
+  const result = await User.findByIdAndUpdate(
+    userId,
+    { $addToSet: { shoppingList: ingredient } },
+    { new: true }
+  );
+
+  // Якщо не вдалось записати викидаємо помилку
+  if (!result) {
+    res.status(400);
+    throw new Error("Bad Request");
+  }
+
+  // Якщо вдалося записати повертаємо результат
+  res.status(200).json({
+    code: 200,
+    message: "success",
+    data: result.shoppingList,
+  });
+};
+module.exports = addToShoppingList;
--- routes/api/shopping-list.js
+++ routes/api/shopping-list.js
@@ -1,5 +1,12 @@
 const express = require("express");
+// const { authenticate } = require("../../middlewares");
+// const { ctrlWrapper } = require("../../helpers");
 
 const shoppingListRouter = express.Router();
 
+// shoppingListRouter.post(
+//   "/add-ingredient/:id",
+//   authenticate,
+//   ctrlWrapper(ctrlRecipes.addFavoriteRecipe)
+// );
 module.exports = shoppingListRouter;
