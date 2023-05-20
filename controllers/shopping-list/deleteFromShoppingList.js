554aec817e730fb4518fc42d0fd310e037ae42b2
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-05-20 14:44:29 -0400

    updateshoppinglist

--- controllers/shopping-list/deleteFromShoppingList.js
+++ controllers/shopping-list/deleteFromShoppingList.js
@@ -4,18 +4,18 @@ const deleteFromShoppingList = async (req, res) => {
   const { ingredientId } = req.query;
   const { user } = req;
 
-  console.log(user._id);
-  console.log(ingredientId);
+  //   console.log(user._id);
+  //   console.log(ingredientId);
   const updateUser = await User.findByIdAndUpdate(
     user._id,
     {
       $pull: {
-        shoppingList: { $elemMatch: { id: ingredientId } },
+        shoppingList: { id: ingredientId },
       },
     },
     { new: true }
   );
-
+  console.log(updateUser);
   res.status(200).json({
     code: 200,
     message: "Deleted",
