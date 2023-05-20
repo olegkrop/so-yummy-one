e5b4e3aa69e30891dad51f5054275aff75d9c527
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-05-20 09:25:14 +0300

    add: pagination helper

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const RequestError = require("./RequestError");
 const ctrlWrapper = require("./ctrlWrapper");
 const sendEmail = require("./sendEmail");
+const paginatedResponse = require("./paginatedResponse");
 
 module.exports = {
   RequestError,
   ctrlWrapper,
   sendEmail,
+  paginatedResponse,
 };
